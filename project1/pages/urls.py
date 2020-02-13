64922ae643425eb9179e3bccbabee6114333c3d8
sri charan <dgscharan@gmail.com>
2020-02-13 12:57:09 +0530

    with html templates

--- project1/pages/urls.py
+++ project1/pages/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.homePageView, name='home')
+    path('', views.HomePageView.as_view(), name='home'),
+    path('about/', views.AboutPageView.as_view(), name='about'),
 ]
-
 
